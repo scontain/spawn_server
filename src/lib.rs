41242fb58f143a11f5027b096b39f912b65d6ba5
Karin Fetzer <karin.fetzer@scontain.com>
2025-08-24 09:12:41 +0200

    chore: remove unwrap and add log output
    Signed-off-by: Karin Fetzer <karin.fetzer@scontain.com>

--- src/lib.rs
+++ src/lib.rs
@@ -24,34 +24,46 @@ pub fn sync_remote_execute(cmd: &str) -> (i32, String, String) {
     use std::time::Duration;
     let very_long_timeout = Duration::new(60 * 60 * 24, 0); // 24h
 
-    let client = reqwest::blocking::Client::builder()
+    let client_result = reqwest::blocking::Client::builder()
         .timeout(very_long_timeout)
-        .build()
-        .unwrap(); // todo: remove unwrap
+        .build();
+    let client = match client_result {
+        Ok(c) => c,
+        Err(e) => {
+            eprintln!("sync_remote_execute: Failed to build client: {}", e);
+            return (
+                -3,
+                "".to_string(),
+                format!("sync_remote_execute API: Client Build Error (ERROR 910-21087-27552). Error: {e}"),
+            );
+        }
+    };
 
     match client.post(get_spawner_url()).json(&cmd).send() {
         Ok(resp) => {
             if resp.status().is_success() {
                 let result: CommandResponse = resp.json().unwrap();
+                println!("sync_remote_execute API: Success - received code {}", result.code);
                 (result.code, result.stdout, result.stderr)
             } else {
+                let result: CommandResponse = resp.json().unwrap();
                 eprintln!("sync_remote_execute API: return status indicates no success (ERROR 8192-173-10620)");
                 (
                     -2,
-                    "".to_string(),
-                    "No Success Error (ERROR 8192-173-10620)".to_string(),
+                    format!("{}", result.stdout),
+                    format!("sync_remote_execute API: No Success Error (ERROR 8192-173-10620): {}", result.stderr),
                 )
             }
         }
         Err(e) => {
             eprintln!(
-                "sync_remote_execute API response cannot be parsed! {} (ERROR 67132-2323-78223)",
+                "sync_remote_execute API: response cannot be parsed! {} (ERROR 67132-2323-78223)",
                 e
             );
             (
                 -1,
                 "".to_string(),
-                "RPC Error  (ERROR 67132-2323-78224)".to_string(),
+                format!("sync_remote_execute API: RPC Error (ERROR 67132-2323-78224): {}",  e),
             )
         }
     }
