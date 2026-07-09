0738b4a9db5ecdae4b5820cad15e14fef722c8e9
dandax123 <daniel@scontain.com>
2026-07-09 08:34:16 +0000

    feat: add *_or_local_exec macros

--- examples/async.rs
+++ examples/async.rs
@@ -1,7 +1,7 @@
 // Simple example of how to use the synchronous client API to
 // talk to the spawn_server
 
-use spawn_server::{SpawnServerCommandResponse, arpc_exec, arpc_sh};
+use spawn_server::{SpawnServerCommandResponse, arpc_or_local_exec, arpc_or_local_sh};
 
 #[tokio::main]
 async fn main() {
@@ -9,26 +9,26 @@ async fn main() {
         exit_code,
         stdout,
         stderr,
-    } = arpc_sh!("ls -lrt").await;
+    } = arpc_or_local_sh!("ls -lrt").await;
     println!("Async:\n - code={exit_code}\n - stdout={stdout}\n - stderr={stderr}");
 
     let SpawnServerCommandResponse {
         exit_code,
         stdout,
         stderr,
-    } = arpc_exec!("ls", &["-lrt"]).await;
+    } = arpc_or_local_exec!("ls", &["-lrt"]).await;
     println!("Async:\n - code={exit_code}\n - stdout={stdout}\n - stderr={stderr}");
 
-    // arpc_exec with env
-    arpc_exec!(
+    // arpc_or_local_exec with env
+    arpc_or_local_exec!(
         "ls",
         &["-la", ">", "ls.log"],
         [("TEST_VAR".to_string(), "value1".to_string())]
     )
     .await;
 
-    // arpc_exec with env_remove and env. This will remove TEST_VAR from the environment, and add it again with the new value.
-    arpc_exec!(
+    // arpc_or_local_exec with env_remove and env. This will remove TEST_VAR from the environment, and add it again with the new value.
+    arpc_or_local_exec!(
         "ls",
         &["-la", ">", "ls.log"],
         [("TEST_VAR".to_string(), "value2".to_string())],
